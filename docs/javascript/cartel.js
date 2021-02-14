5430b5a1521992ac91ce265ed683e400e9803e91
Mendieta Sergio <mendieta.sergio.1995@gmail.com>
2021-02-14 05:46:32 -0300

    Añade cambios, responsive, menu desplegable

--- docs/javascript/cartel.js
+++ docs/javascript/cartel.js
@@ -1,3 +1,13 @@
-window.addEventListener('load',() =>{
+addEventListener('DOMContentLoaded',() =>{
     alert('¡¡Página en Proceso!!')
+    const btn_menu = document.querySelector('.btn_menu')
+    //si es verdadero, entra en la condicion
+    if(btn_menu){
+        //cuando haga click sobre btn_menu, se activa el evento
+        btn_menu.addEventListener('click',()=>{
+            const menu_items = document.querySelector('.menu_items')
+            //Si no tiene la clase show, lo agrega
+            menu_items.classList.toggle('show')
+        })
+    }
 })
